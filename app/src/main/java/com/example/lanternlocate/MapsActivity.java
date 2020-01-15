aa0a297f8a9099653b542a965a4dc0f1d6d54e26
Demo <demo@wc137-dhcp-205.brynmawr.edu>
2020-01-15 15:26:50 -0500

    Maps activity v1

--- app/src/main/java/com/example/lanternlocate/MapsActivity.java
+++ app/src/main/java/com/example/lanternlocate/MapsActivity.java
@@ -1,8 +1,13 @@
 package com.example.lanternlocate;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,18 +16,34 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final String TOAST_TEXT = "Activate Location.";
+
+    private Location currentLocation;
+    private double currentLat;
+    private double currentLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
     }
 
 
@@ -35,13 +56,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+
+                            // Logic to handle location object
+                            currentLat = location.getLatitude();
+                            currentLng = location.getLongitude();
+
+                            LatLng brynMawr = new LatLng( currentLat, currentLng );
+
+                            mMap.addMarker(new MarkerOptions().position(brynMawr).title("Marker in Bryn Mawr"));
+
+                            float zoomLevel = 16.0f;
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(brynMawr, zoomLevel));
+
+                            Log.i("FARYAL1", Double.toString(currentLat));
+                            Log.i("FARYAL1", Double.toString(currentLng));
+                        }
+                    }
+                });
+
+
+        fusedLocationClient.getLastLocation().addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Context context = getApplicationContext();
+                CharSequence text = "Activate Location Services.";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
+        //LatLng brynMawr = new LatLng( 40.0289998, -75.3126857 );
+
+        Log.i("FARYAL2", Double.toString(currentLat));
+        Log.i("FARYAL2", Double.toString(currentLng));
+
+
+
+
     }
+
+
+
+
 }
